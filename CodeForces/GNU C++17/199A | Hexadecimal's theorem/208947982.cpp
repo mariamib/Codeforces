50415f037f7a9e5e2e59c652bd4c609b1ea67430
Mariam Ibrahem <70824552+mariamib@users.noreply.github.com>
2023-07-20 04:53:27 +0300

    Committing files

---
+++ CodeForces/GNU C++17/199A | Hexadecimal's theorem/208947982.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<cmath>
+#include<sstream>#include <string>
+#include<map>
+#include<stack>
+int main()
+{
+ long long n;
+ cin >> n;
+ cout << n << " " << 0 << " " << 0;
+}
