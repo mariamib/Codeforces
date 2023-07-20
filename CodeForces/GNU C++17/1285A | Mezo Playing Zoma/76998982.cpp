d079c67de8f0e629e2fe864af2c2f0c9f8bc1941
Mariam Ibrahem <70824552+mariamib@users.noreply.github.com>
2023-07-20 04:58:33 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1285A | Mezo Playing Zoma/76998982.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int num,countcounterr=0;cin>>num;
+    char a;
+    for(int i=0;i<num;i++)
+    {
+        cin>>a;
+ 
+    }
+    cout<<num+1;
+}
