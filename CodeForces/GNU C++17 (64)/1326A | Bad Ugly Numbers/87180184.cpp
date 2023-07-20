1b5f3e918d9ba7b791b5952b42963f44ef79cae1
Mariam Ibrahem <70824552+mariamib@users.noreply.github.com>
2023-07-20 04:57:27 +0300

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1326A | Bad Ugly Numbers/87180184.cpp
@@ -0,0 +1,31 @@
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include <algorithm> 
+#include <iostream> 
+#include<vector>
+#include<iomanip>
+#include <regex>
+ 
+using namespace std;
+int main()
+{
+    long long n, num;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> num;
+        if (num == 1)
+            cout << -1;
+        else
+        {
+            cout << 2;
+            for (long long i = 1; i < num; i++)
+            {
+                cout << 7;
+                
+           }
+        }
+        cout << endl;
+    }
+}
