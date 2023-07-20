d1425457b7470eb49a743503116d32b65e4f30b9
Mariam Ibrahem <70824552+mariamib@users.noreply.github.com>
2023-07-20 05:00:36 +0300

    Committing files

---
+++ CodeForces/GNU C++17/37A | Towers/74659804.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int num,a,x;cin>>num;
+    map<int ,int>arr;
+    map<int,int>::iterator it;
+    int counter=0,b=0;
+    for(int i=0;i<num;i++)
+    {
+        cin>>a;
+        it=arr.find(a);
+        if(it==arr.end())
+        {
+            arr[a]=1;
+            counter++;
+        }
+        else
+        {
+            arr[a]++;
+        }
+ 
+            if(b<arr[a])
+                b=arr[a];
+    }
+   cout<<b<<" "<<counter;
+ 
+    return 0;
+}
