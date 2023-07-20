ea0c2589eb1b045cfd88d76a181a660607efbd74
Mariam Ibrahem <70824552+mariamib@users.noreply.github.com>
2023-07-20 04:56:59 +0300

    Committing files

---
+++ CodeForces/GNU C++17 (64)/460A | Vasya and Socks/89326975.cpp
@@ -0,0 +1,27 @@
+ 
+#include <iostream>
+#include<sstream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<cmath>
+#include <cstdio>
+#include <cinttypes>
+using namespace std;
+ 
+int main()
+{
+ 
+ int  num1, num2;
+ cin >> num1 >> num2;
+ cout<<num1+ ((num1 - 1) /( num2 - 1));
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
