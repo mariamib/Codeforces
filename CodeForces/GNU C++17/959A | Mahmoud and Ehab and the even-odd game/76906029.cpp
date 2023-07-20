9028d4b4633e1e93bb1e8dc1c8a474eeead76244
Mariam Ibrahem <70824552+mariamib@users.noreply.github.com>
2023-07-20 04:58:51 +0300

    Committing files

---
+++ CodeForces/GNU C++17/959A | Mahmoud and Ehab and the even-odd game/76906029.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    long long num;cin>>num;
+    if(num%2==0)
+    cout<<"Mahmoud";
+    else
+        cout<<"Ehab";
+    return 0;
+}
