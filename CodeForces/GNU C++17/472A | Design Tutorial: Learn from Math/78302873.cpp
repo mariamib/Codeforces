df34b82f0a93db964bcb1fd199ca44d8be068c40
Mariam Ibrahem <70824552+mariamib@users.noreply.github.com>
2023-07-20 04:57:43 +0300

    Committing files

---
+++ CodeForces/GNU C++17/472A | Design Tutorial: Learn from Math/78302873.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+ int main()
+ {
+    long long num;cin>>num;
+    if(num%2==0)
+        cout<<num-4<<" "<<4;
+    else
+        cout<<num-9<<" "<<9;
+ 
+ }
